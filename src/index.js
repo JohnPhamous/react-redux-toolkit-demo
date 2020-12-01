1c5df257a77eee62b1388ecf2c5869511330c83e
John Pham <johnphammail@gmail.com>
2020-11-30 17:56:51 -0800

    Make grid responsive

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./app/App";
 import store from "./app/store";
 import { Provider } from "react-redux";
